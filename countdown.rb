085e4c10948b8e34c9b80562a95e56214353c972
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-25 05:47:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,13 @@
 #write your code here
 
-def countdown
-  number = 12
+def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  
 end
