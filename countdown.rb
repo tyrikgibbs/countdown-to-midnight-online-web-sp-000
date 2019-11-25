872b74207b4d6f17b0f24d13c1d8a19af30cfa6d
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-25 05:40:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
-def countdown(number)
+def countdown
+  number = 12
   while number >= 0
     puts "#{number} SECOND(S)!"
     x -= 1
   end
-  return "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"  
 end
